fd10121c66a9e5a704708b59ea73529e55c6a9e5
Abdennour Tadjer <abdennour.tadjer@gmail.com>
2024-11-22 01:09:25 +0100

    updating routes entry to set domain foreach route entry in the central app. make the tenant routes accessible by subdomains, custom domains.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,27 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        // web: __DIR__.'/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        using: function () {
+            foreach (config('tenancy.central_domains') as $domain) {
+                Route::middleware('web')->domain($domain)->group(base_path('routes/web.php'));
+
+                Route::middleware('web')->domain('admin.' . $domain)->group(base_path('routes/admin.php'));
+            }
+
+            Route::middleware('web')->group(base_path('routes/tenant.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
