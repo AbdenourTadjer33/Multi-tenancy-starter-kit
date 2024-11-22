a55aa97b9bdb071525afdb4bf328d355c71126de
Abdennour Tadjer <abdennour.tadjer@gmail.com>
2024-11-22 02:13:36 +0100

    adding blade views

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $title ?? config('app.name') }}</title>
+
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css'])
+    @endif
+
+</head>
+
+<body class="font-sans antialiased bg-red-50">
+    {{ $slot }}
+</body>
+
+</html>
